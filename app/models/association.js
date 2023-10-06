56103e3773c1f616de1f9ae4a3ac7b5b35a8a7a0
Andres Vergauwen <andres.vergauwen@hotmail.com>
2023-10-06 11:54:46 +0200

    Add multiple sites to association

--- app/models/association.js
+++ app/models/association.js
@@ -4,5 +4,6 @@ export default class AssociationModel extends Model {
   @attr name;
   @attr description;
   @hasMany('identifier', { inverse: null, async: true }) identifiers;
+  @hasMany('site', { inverse: null, async: true }) sites;
   @belongsTo('site', { inverse: null, async: true }) primarySite;
 }
