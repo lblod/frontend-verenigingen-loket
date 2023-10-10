52136bda896b4b6ac6fe48d3ed3ca6e60b49cb76
Andres Vergauwen <andres.vergauwen@hotmail.com>
2023-10-10 10:27:34 +0200

    Add pagination to the table

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,10 +5,21 @@ export default class IndexRoute extends Route {
   @service session;
   @service store;
 
+  queryParams = {
+    size: { refreshModel: true },
+    page: {
+      refreshModel: true,
+    },
+  };
+
   async beforeModel(transition) {
     this.session.requireAuthentication(transition, 'auth.login');
   }
-  async model() {
-    return this.store.findAll('association');
+  async model(params) {
+    const query = {
+      sort: params.sort ?? 'name',
+      page: { size: 20, number: params.page },
+    };
+    return this.store.query('association', query);
   }
 }
