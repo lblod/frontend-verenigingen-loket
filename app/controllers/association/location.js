818b321e608215910491987871b99175a74d129c
Andres Vergauwen <andres.vergauwen@hotmail.com>
2023-10-19 12:25:55 +0200

    Remove combineFullAddress function

--- app/controllers/association/location.js
+++ app/controllers/association/location.js
@@ -1,9 +1,6 @@
 import Controller from '@ember/controller';
 import { A } from '@ember/array';
-import { tracked } from '@glimmer/tracking';
-import { combineFullAddress } from '../../models/address';
 export default class AssociationLocationController extends Controller {
-  @tracked showTableLoader = true;
   get sites() {
     if (this.model.sites) {
       const sites = this.model.sites;
@@ -12,13 +9,8 @@ export default class AssociationLocationController extends Controller {
         primarySite.address.isPrimary = true;
       }
       const mergedSites = A([primarySite, ...sites]);
-      this.showTableLoader = false;
       return mergedSites;
     }
     return null;
-  }
-
-  getFullAddress(address) {
-    return combineFullAddress(address);
   }
 }
--- app/models/address.js
+++ app/models/address.js
@@ -11,27 +11,3 @@ export default class AddressModel extends Model {
   @attr country;
   @attr fullAddress;
 }
-
-export function combineFullAddress(address) {
-  if (!address) return null;
-
-  const fullStreet = [address.street, address.number, address.boxNumber]
-    .filter(Boolean)
-    .join(' ')
-    .trim();
-
-  const municipalityInformation = [address.postcode, address.municipality]
-    .filter(Boolean)
-    .join(' ')
-    .trim();
-
-  const countryInformation = address.country || '';
-
-  const parts = [
-    fullStreet,
-    municipalityInformation,
-    countryInformation,
-  ].filter(Boolean);
-
-  return parts.length ? parts.join(', ') : null;
-}
