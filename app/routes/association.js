73e8e93f5b8eacfc56cc7e13626aa155cd29b3ff
Andres Vergauwen <andres.vergauwen@hotmail.com>
2023-10-24 11:45:52 +0200

    Redirect to login if no authentication

--- app/routes/association.js
+++ app/routes/association.js
@@ -2,7 +2,11 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default class AssociationRoute extends Route {
+  @service session;
   @service store;
+  async beforeModel(transition) {
+    this.session.requireAuthentication(transition, 'auth.login');
+  }
   model({ id }) {
     return this.store.findRecord('association', id);
   }
