6a82217ce348ff88d5e685b743a3608990e75357
Andres Vergauwen <andres.vergauwen@hotmail.com>
2023-10-25 13:02:15 +0200

    Add filters for activities and status code

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,32 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
-
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 export default class IndexController extends Controller {
+  @service router;
   @tracked page = 0;
   @tracked size = 20;
   @tracked sort = 'name';
+  @tracked selectedActivities = [];
+  @tracked activities = '';
+  @tracked selectedOrganizationStatus = [];
+  @tracked status = '';
+
+  queryParams = ['sort', 'page', 'search', 'activities', 'status'];
+
+  @action
+  setActivities(selectedActivities) {
+    this.page = 0;
+    this.selectedActivities = selectedActivities;
+    this.activities = selectedActivities
+      .map((activity) => activity.id)
+      .join(',');
+  }
+
+  @action
+  setOrganizationStatus(selectedStatus) {
+    this.page = 0;
+    this.selectedOrganizationStatus = selectedStatus;
+    this.status = selectedStatus.join(',');
+  }
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+
 export default class IndexRoute extends Route {
   @service currentSession;
   @service session;
@@ -8,14 +9,15 @@ export default class IndexRoute extends Route {
   queryParams = {
     sort: { refreshModel: true },
     size: { refreshModel: true },
-    page: {
-      refreshModel: true,
-    },
+    page: { refreshModel: true },
+    activities: { refreshModel: true },
+    status: { refreshModel: true },
   };
 
   async beforeModel(transition) {
     this.session.requireAuthentication(transition, 'auth.login');
   }
+
   async model(params) {
     const include = [
       'primary-site.address',
@@ -23,11 +25,39 @@ export default class IndexRoute extends Route {
       'organization-status',
       'activities',
     ].join(',');
-    const query = {
-      sort: params.sort ? `${params.sort},name` : 'name',
-      page: { size: 20, number: params.page },
-      include,
+
+    const query = buildQuery(params, include);
+    const [associations, activities, organizationStatus] = await Promise.all([
+      this.store.query('association', query),
+      this.store.query('activity', { sort: 'label' }),
+      this.store.query('organization-status-code', { sort: 'label' }),
+    ]);
+
+    return {
+      associations,
+      filters: {
+        activities,
+        organizationStatus,
+      },
     };
-    return this.store.query('association', query);
   }
+}
+
+function buildQuery(params, include) {
+  const query = {
+    sort: params.sort ? `${params.sort},name` : 'name',
+    page: { size: 20, number: params.page },
+    include,
+    filters: {},
+  };
+
+  if (params.activities !== '') {
+    query.filters.activities = { ':id:': params.activities };
+  }
+
+  if (params.status !== '') {
+    query.filters['organization-status'] = { ':id:': params.status };
+  }
+
+  return query;
 }
