c753b0f64e675b30e0e83bb7743f92fa75e20cb7
Andres Vergauwen <andres.vergauwen@hotmail.com>
2023-11-28 16:57:13 +0100

    Add loading to contact-detail and location

--- app/controllers/association/contact-detail.js
+++ app/controllers/association/contact-detail.js
@@ -2,4 +2,13 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 export default class AssociationContactDetailController extends Controller {
   @service contactPoints;
+  get isLoading() {
+    return this.model.association.isRunning;
+  }
+
+  get association() {
+    return this.model.association.isFinished
+      ? this.model.association.value
+      : null;
+  }
 }
--- app/routes/association/contact-detail.js
+++ app/routes/association/contact-detail.js
@@ -1,13 +1,21 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { keepLatestTask } from 'ember-concurrency';
 export default class AssociationContactDetailRoute extends Route {
   @service store;
   async model() {
+    return {
+      association: this.loadAssociation.perform(),
+    };
+  }
+
+  @keepLatestTask({ cancelOn: 'deactivate' })
+  *loadAssociation() {
     const { id } = this.paramsFor('association');
     const include = ['contact-points', 'primary-site.address'].join(',');
     const query = {
       include,
     };
-    return this.store.findRecord('association', id, query);
+    return yield this.store.findRecord('association', id, query);
   }
 }
