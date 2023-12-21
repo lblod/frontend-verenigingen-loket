f3856cbcf0a4f22f53796080ee4e7cda9016496f
Andres Vergauwen <andres.vergauwen@hotmail.com>
2023-12-21 13:27:43 +0100

    Remove unused store and router

--- app/routes/association/recognition/create.js
+++ app/routes/association/recognition/create.js
@@ -2,8 +2,6 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default class AssociationRecognitionCreateRoute extends Route {
-  @service store;
-  @service router;
   @service currentRecognition;
   async model() {
     return this.currentRecognition.setCurrentRecognition(null);
