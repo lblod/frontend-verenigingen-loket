54b83d7c3c01ec18b80cf4a8978bfd6eda735884
Andres Vergauwen <andres.vergauwen@hotmail.com>
2023-12-21 13:25:47 +0100

    Remove unused get recognition

--- app/controllers/association/recognition/edit.js
+++ app/controllers/association/recognition/edit.js
@@ -4,10 +4,4 @@ import { inject as service } from '@ember/service';
 export default class AssociationRecognitionEditController extends Controller {
   @service currentRecognition;
   @service currentAssociation;
-
-  get recognition() {
-    return this.model.recognition?.isFinished
-      ? this.model.recognition.value
-      : null;
-  }
 }
