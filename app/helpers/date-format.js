826bd10ae951981b4f8d1d7f6cdd8b9656e2de4e
Andres Vergauwen <andres.vergauwen@hotmail.com>
2024-01-03 12:14:15 +0100

    Fix get currentRecognition on general page based on today

--- app/helpers/date-format.js
+++ app/helpers/date-format.js
@@ -1,10 +1,13 @@
 import { helper } from '@ember/component/helper';
 
-export default helper(function DateFormat([date]) {
+export default helper(function DateFormat([date, format = 'DD-MM-YYYY']) {
   const newDate = new Date(date);
   const day = newDate.toLocaleDateString('nl-BE', { day: '2-digit' }),
     month = newDate.toLocaleDateString('nl-BE', { month: '2-digit' }),
     year = newDate.getFullYear();
 
+  if (format === 'YYY-MM-DD') {
+    return year + '-' + month + '-' + day;
+  }
   return day + '-' + month + '-' + year;
 });
--- app/routes/association/general.js
+++ app/routes/association/general.js
@@ -1,9 +1,11 @@
 import Route from '@ember/routing/route';
 import { keepLatestTask } from 'ember-concurrency';
 import { inject as service } from '@ember/service';
+import dateFormat from '../../helpers/date-format';
 export default class AssociationGeneralRoute extends Route {
   @service session;
   @service store;
+
   async model() {
     const { id } = this.paramsFor('association');
     const association = await this.store.findRecord('association', id);
@@ -15,14 +17,30 @@ export default class AssociationGeneralRoute extends Route {
 
   @keepLatestTask({ cancelOn: 'deactivate' })
   *loadRecognition(id) {
-    return yield this.store.query('recognition', {
+    const today = dateFormat.compute([new Date(), 'YYY-MM-DD']);
+    const [currentRecognition, lastRecognition] = yield Promise.all([
+      this.getRecognition(id, today),
+      this.getRecognition(id),
+    ]);
+    if (currentRecognition.length > 0) return currentRecognition;
+    return lastRecognition;
+  }
+  async getRecognition(id, date) {
+    const query = {
       include: ['awarded-by', 'validity-period'].join(','),
       filter: {
         association: {
-          id: id,
+          id,
         },
       },
       sort: `-validity-period.end-time`,
-    });
+    };
+    if (date) {
+      query.filter['validity-period'] = {
+        ':lte:start-time': date,
+        ':gte:end-time': date,
+      };
+    }
+    return await this.store.query('recognition', query);
   }
 }
