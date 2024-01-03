a6d5e2c5dc62d665fefc0c6e32402579b2d92ca1
Andres Vergauwen <andres.vergauwen@hotmail.com>
2024-01-03 15:56:16 +0100

    Remove duplicate Date declaration and use it inside if

--- app/helpers/date-format.js
+++ app/helpers/date-format.js
@@ -1,8 +1,8 @@
 import { helper } from '@ember/component/helper';
 
 export default helper(function DateFormat([date]) {
-  if (!isNaN(new Date(date))) {
-    const newDate = new Date(date);
+  const newDate = new Date(date);
+  if (!isNaN(newDate)) {
     const day = newDate.toLocaleDateString('nl-BE', { day: '2-digit' }),
       month = newDate.toLocaleDateString('nl-BE', { month: '2-digit' }),
       year = newDate.getFullYear();
