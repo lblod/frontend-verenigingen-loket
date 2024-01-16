816655e0edf3f38310b042fda112503c7a3d37ef
Andres Vergauwen <andres.vergauwen@hotmail.com>
2024-01-16 11:21:48 +0100

    Add default sort for recognition

--- app/controllers/association/recognition/index.js
+++ app/controllers/association/recognition/index.js
@@ -1,7 +1,10 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 export default class AssociationRecognitionController extends Controller {
   @service() currentAssociation;
+  @tracked sort = '-validity-period.end-time';
+
   get isLoading() {
     return this.model.recognitions.isRunning;
   }
