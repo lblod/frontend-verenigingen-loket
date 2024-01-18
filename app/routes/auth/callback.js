6f69cb7e325523d351a918b679cf7cedb9515535
Andres Vergauwen <andres.vergauwen@hotmail.com>
2024-01-18 09:58:26 +0100

    Update callback

--- app/routes/auth/callback.js
+++ app/routes/auth/callback.js
@@ -5,14 +5,16 @@ export default class AuthCallbackRoute extends Route {
   @service session;
   @service router;
 
+  queryParams = ['code'];
   beforeModel() {
     this.session.prohibitAuthentication('index');
   }
 
-  async model({ code }) {
-    if (code) {
+  async model() {
+    console.log(this.code);
+    if (this.code) {
       try {
-        await this.session.authenticate('authenticator:acm-idm', code);
+        await this.session.authenticate('authenticator:acm-idm', this.code);
       } catch (error) {
         throw new Error(
           'Something went wrong while authenticating the user in the backend. The token might be expired.',
