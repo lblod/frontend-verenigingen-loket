1e4de69406a836701351a3790bddac40cc3f506a
Andres Vergauwen <andres.vergauwen@hotmail.com>
2024-01-23 12:12:50 +0100

    WIP: Fetch logout url instead of redirecting to it

--- app/routes/auth/logout.js
+++ app/routes/auth/logout.js
@@ -1,6 +1,5 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import ENV from 'frontend-verenigingen-loket/config/environment';
 
 export default class AuthLogoutRoute extends Route {
   @service router;
@@ -9,13 +8,16 @@ export default class AuthLogoutRoute extends Route {
   async beforeModel(transition) {
     if (this.session.requireAuthentication(transition, 'login')) {
       try {
-        let wasMockLoginSession = this.session.isMockLoginSession;
+        const wasMockLoginSession = this.session.isMockLoginSession;
         await this.session.invalidate();
-        let logoutUrl = wasMockLoginSession
-          ? this.router.urlFor('mock-login')
-          : ENV.acmidm.logoutUrl;
-
-        window.location.replace(logoutUrl);
+        if (wasMockLoginSession) {
+          await fetch('https://authenticatie-ti.vlaanderen.be/op/v1/logout')
+            .then(() => {})
+            .catch((error) => {
+              console.error(error);
+            });
+        }
+        return window.location.replace(this.router.urlFor('login'));
       } catch (error) {
         throw new Error(
           'Something went wrong while trying to remove the session on the server',
