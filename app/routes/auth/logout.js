ee36bb470cce81f50b9f7f69b4aed2a413b71df2
Andres Vergauwen <andres.vergauwen@hotmail.com>
2024-01-23 13:18:45 +0100

    Fix logout

--- app/routes/auth/logout.js
+++ app/routes/auth/logout.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import ENV from 'frontend-verenigingen-loket/config/environment';
 
 export default class AuthLogoutRoute extends Route {
   @service router;
@@ -8,16 +9,13 @@ export default class AuthLogoutRoute extends Route {
   async beforeModel(transition) {
     if (this.session.requireAuthentication(transition, 'login')) {
       try {
-        const wasMockLoginSession = this.session.isMockLoginSession;
+        let wasMockLoginSession = this.session.isMockLoginSession;
         await this.session.invalidate();
-        if (!wasMockLoginSession) {
-          await fetch('https://authenticatie-ti.vlaanderen.be/op/v1/logout')
-            .then(() => {})
-            .catch((error) => {
-              console.error(error);
-            });
-        }
-        return window.location.replace(this.router.urlFor('login'));
+        let logoutUrl = wasMockLoginSession
+          ? this.router.urlFor('login')
+          : ENV.acmidm.logoutUrl;
+
+        window.location.replace(logoutUrl);
       } catch (error) {
         throw new Error(
           'Something went wrong while trying to remove the session on the server',
