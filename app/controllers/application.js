93160fab50c856d7272541a9e801c0b04b28ec4c
Andres Vergauwen <andres.vergauwen@hotmail.com>
2024-06-19 15:08:44 +0200

    Feat: environment banner added

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -9,4 +9,57 @@ export default class ApplicationController extends Controller {
   @service store;
   @service router;
   appTitle = 'Verenigingen';
+
+  get isLocalhost() {
+    if (
+      window.location.hostname === 'localhost' ||
+      window.location.hostname === '[::1]'
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  get environmentName() {
+    const thisEnvironmentName = this.isLocalhost
+      ? 'local'
+      : getOwner(this).resolveRegistration('config:environment')
+          .environmentName;
+
+    return thisEnvironmentName;
+  }
+
+  get environmentInfo() {
+    let environment = this.environmentName;
+    switch (environment) {
+      case 'QA':
+        return {
+          title: 'testomgeving',
+          skin: 'warning',
+        };
+      case 'DEV':
+        return {
+          title: 'ontwikkelomgeving',
+          skin: 'success',
+        };
+      case 'local':
+        return {
+          title: 'lokale omgeving',
+          skin: 'error',
+        };
+      default:
+        return {
+          title: '',
+        };
+    }
+  }
+
+  get showEnvironment() {
+    return (
+      this.environmentName !== '' &&
+      this.environmentInfo.title !== '' &&
+      this.environmentName !== '{{ENVIRONMENT_NAME}}'
+    );
+  }
 }
