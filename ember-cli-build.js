99ac3ee24a94cf51cf8dfb8abdbbf2466b63c5ff
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2025-07-10 18:41:38 +0200

    Update ember-concurrency and ember-power-select
    Both need to be updated at the same time since ember-power-select v8 doesn't support older ember-concurrency versions.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
+    babel: {
+      plugins: [
+        require.resolve('ember-concurrency/async-arrow-task-transform'),
+      ],
+    },
     emberData: {
       deprecations: {
         DEPRECATE_STORE_EXTENDS_EMBER_OBJECT: false,
