a58cb471c07eb58edbf5910f0a4e60f7ba848570
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2025-07-10 19:24:31 +0200

    Update to Ember 6.4 LTS

--- app/app.js
+++ app/app.js
@@ -1,10 +1,14 @@
 import Application from '@ember/application';
+import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import ENV from 'frontend-verenigingen-loket/config/environment';
 import { silenceEmptySyncRelationshipWarnings } from './utils/ember-data';
 
-silenceEmptySyncRelationshipWarnings();
+if (macroCondition(isDevelopingApp())) {
+  importSync('./deprecation-workflow');
+  silenceEmptySyncRelationshipWarnings();
+}
 
 export default class App extends Application {
   modulePrefix = ENV.modulePrefix;
