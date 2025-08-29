ddd0bb53c0b1c44ec8d469ec00f9d60932eae13f
benjay10 <ben.behaeghe@redpencil.io>
2025-08-29 02:16:27 +0200

    Create Role model and relate to Membership

---
+++ app/models/role.js
@@ -0,0 +1,16 @@
+import { attr } from '@ember-data/model';
+import ConceptModel from './concept';
+
+export const ROLES = {
+  PRIMARY: 'http://lblod.data.gift/concepts/75e74415-35cf-4da5-bac5-b72a1c137799',
+  SECONDARY: 'http://lblod.data.gift/concepts/78451ac5-ec0b-469d-b918-0a8ef92a77b2',
+}
+
+export default class RoleModel extends ConceptModel {
+
+  @attr uri;
+
+  get isPrimary() {
+    return this.uri === ROLES.PRIMARY;
+  }
+}
