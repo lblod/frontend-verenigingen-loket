1d9e37c4fabb2f08562c8dc2a4bff9912c05ce10
benjay10 <ben.behaeghe@redpencil.io>
2025-09-19 03:07:40 +0200

    Rename helper file, change to role checking
    The name now does not follow conventions, but it wouldn't compile
    otherwise. It worked at first, but after a merge with unrelated
    code chages, the project suddenly wouldn't compile because of this. I
    really have no clue as to why the original name is no longer working.
    Also changed the role checking on the role itself, instead of on the
    representative, because all of a sudden, working with promises wouldn't
    work any longer. This was also really weird.

--- app/helpers/is-primary-representative.ts
+++
@@ -1,7 +0,0 @@
-import { ROLES } from '../models/role';
-import type Membership from '../models/membership';
-
-
-export default async function isPrimaryRepresentative(representative: Membership) {
-  return (await representative.role).uri === ROLES.PRIMARY;
-}
