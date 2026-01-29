c96b270220b3597cb16dc47955e9728993dc4530
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2026-01-29 15:46:24 +0100

    Move the recognition-validation code to the corresponding model class
    This is more consistent with how we store other validations.

--- app/models/recognition.js
+++ app/models/recognition.js
@@ -1,4 +1,5 @@
 import Model, { attr, belongsTo } from '@ember-data/model';
+import Joi from 'joi';
 
 export const AWARDED_BY_OPTIONS = {
   COLLEGE: 'College van burgemeester en schepenen',
@@ -47,3 +48,57 @@ export default class RecognitionModel extends Model {
   })
   file;
 }
+
+const websiteRegex = /^https:\/\//;
+export const validationSchema = Joi.object()
+  .keys({
+    dateDocument: Joi.date()
+      .optional()
+      .messages({ 'date.base': 'Gelieve een geldige datum te kiezen.' }),
+    legalResource: Joi.alternatives().try(
+      Joi.string()
+        .allow(null)
+        .empty('')
+        .pattern(websiteRegex, { name: 'website' })
+        .messages({
+          'string.pattern.name': 'Geef een geldig internetadres in.',
+        }),
+    ),
+    startTime: Joi.date().required().messages({
+      'any.required': 'Gelieve een geldige ingangsdatum te kiezen.',
+      'date.base': 'Gelieve een geldige ingangsdatum te kiezen.',
+      'date.less':
+        'Gelieve een ingangsdatum te kiezen dat voor de einddatum komt.',
+    }),
+    endTime: Joi.date().required().greater(Joi.ref('startTime')).messages({
+      'any.required': 'Gelieve een geldige einddatum te kiezen.',
+      'date.base': 'Gelieve een geldige einddatum te kiezen.',
+      'date.greater':
+        'Gelieve een einddatum te kiezen dat na de ingangsdatum komt.',
+    }),
+    awardedBy: Joi.any().required().messages({
+      'any.required':
+        'Gelieve de entiteit te selecteren die de erkenning toekent.',
+    }),
+    isDelegatedToRequired: Joi.boolean(),
+    delegatedTo: Joi.when('isDelegatedToRequired', {
+      is: true,
+      then: Joi.required().messages({
+        'any.required':
+          'Gelieve de gedelegeerde entiteit te selecteren die de erkenning toekent.',
+      }),
+      otherwise: Joi.allow(null),
+    }),
+    file: Joi.object()
+      .allow(null)
+      .custom((value, helpers) => {
+        if (!(value instanceof File)) {
+          return helpers.message('Gelieve een bestand te kiezen.');
+        }
+        if (!value.name.endsWith('.pdf')) {
+          return helpers.message('Enkel een PDF-bestand is toegelaten.');
+        }
+        return value;
+      }),
+  })
+  .options({ abortEarly: false });
